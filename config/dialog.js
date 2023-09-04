61f9541dc0a9a2d93798ccd2d5dd4d13bb2c243e
Fjean <fjean0522@gmail.com>
2023-09-04 19:36:59 -0400

    Create dialog module.
    Create welcome & goodbye messages & export.
    
    Import into index.js & add welcome message as NodeWise's greeting.

---
+++ config/dialog.js
@@ -0,0 +1,5 @@
+import colors from 'colors';
+
+export const welcomeMessage = colors.green('NodeWise: ') + 'Greetings! How may I assist you?';
+
+export const goodbyeMessage = 'Goodbye! If you ever have more questions or need assistance in the future, feel free to return. Have a great day!';
--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 import openai from './config/openai.js';
 import readlineSync from 'readline-sync';
 import colors from 'colors';
+import { welcomeMessage, goodbyeMessage } from './config/dialog.js';
 
 const runBot = async () => {
-    console.log(colors.green('NodeWise: ') + 'Greetings! How may I assist you?'); 
+    console.log(welcomeMessage); 
 
     while (true) {
         const userInput = readlineSync.question(colors.yellow('You: '));
+
+        try {
+            // Send user input as Request to API
+            const completionRequest = await openai.chat.completions.create({
+                model: 'gpt-3.5-turbo',
+                messages: [{ role: 'user', content: userInput }]
+            });
+
+            // Get Response
+            const completionResponse = completionRequest.choices[0].message.content;
+
+            console.log(colors.green('NodeWise: ') + completionResponse);
+
+        } catch (error) {
+            console.error(colors.red(error));
+        }
     }
 };
 
