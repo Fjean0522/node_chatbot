20213fac3142058aa259a7be9fa5e3023bfb108a
Fjean <fjean0522@gmail.com>
2023-09-04 19:24:27 -0400

    Create NodeWise initial welcome message.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 import colors from 'colors';
 
 const runBot = async () => {
-    
-}
+    console.log(colors.green('NodeWise: ') + 'Greetings! How may I assist you?'); 
+};
 
 runBot();
